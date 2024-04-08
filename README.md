112f8fd961839184dcc6de1bdc43ffce9c71660d
坂本一弥 <sskazuya1130@gmail.com>
2024-04-08 23:08:39 +0900

    feat: create directory

--- README.md
+++ README.md
@@ -1,39 +1,22 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
-
 # frontend
 
 ## 概要
 
 ## 環境
 
-## 環境構築
+## [環境構築](https://www.notion.so/389c047f083e4289b4f9ddae4d737386#3305551ed6564d7ab4ef6bb685e78bb0)
 
-- コンテナ起動
+- bun がインストールされているか確認
 
 ```bash
 $ bun -v
 1.0.2
+```
+
+インストールされていない場合は、以下を実行する
+
+```bash
+$ brew install bun
 ```
 
 - 依存関係をインストール
---
+++ src/features/home/page.tsx
@@ -0,0 +1,69 @@
+export default function Page() {
+  return (
+    <div className="my-12">
+      <div className="w-full rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="text-gray-700 font-bold text-xl mb-2">
+            Scrapタイトル1
+          </div>
+          <p className="text-gray-700 text-base">
+            繰り返されるテキスト繰り返されるテキスト繰り返されるテキスト繰り返されるテキスト繰り返されるテキスト
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Go
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Next.js
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #React
+          </span>
+        </div>
+      </div>
+      <div className="w-full rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="text-gray-700 font-bold text-xl mb-2">
+            Scrapタイトル2
+          </div>
+          <p className="text-gray-700 text-base">
+            繰り返されるテキスト繰り返されるテキスト繰り返されるテキスト繰り返されるテキスト繰り返されるテキスト
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Rust
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Nuxt
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Vue
+          </span>
+        </div>
+      </div>
+      <div className="w-full rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="text-gray-700 font-bold text-xl mb-2">
+            Scrapタイトル3
+          </div>
+          <p className="text-gray-700 text-base">
+            繰り返されるテキスト繰り返されるテキスト繰り返されるテキスト繰り返されるテキスト繰り返されるテキスト
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Ruby
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Rails
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #Vue
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
